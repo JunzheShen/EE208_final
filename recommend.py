0d5d64e23ea9e68310082c33809dec25b7ae6ee1
sjz <2287835421@qq.com>
2022-11-29 07:13:37 +0000

    将推荐标签改为出现次数最多的5个词

--- recommend.py
+++ recommend.py
@@ -7,6 +7,27 @@ def stopwordslist():
     stopwords += ['网易', '体育']
     return stopwords
 
+def frequency_sort(items):
+    # your code here
+    _set = set()
+    for i in items:
+        if i not in _set:
+            _set.add(i)
+    _set = list(_set)
+    lst = []
+    dic = {}
+    # # print(lst1)
+    for i in _set:
+        dic[i] = items.count(i)
+    dic = dict(sorted(dic.items(), key=lambda x:x[1], reverse=True))
+    # #print(dic)
+    count = 0
+    for k, v in dic.items():
+        # for i in range(v):
+            lst.append(k)
+    # # print(lst)
+    return lst
+
 
 def getKey(reslist, keyword, maxiter=100):
     text = ""
@@ -24,12 +45,18 @@ def getKey(reslist, keyword, maxiter=100):
         if word not in stopwords:
             if word != '\t':
                 tmp += word
-    keyWords = analyse.extract_tags(tmp, topK=5, allowPOS=('ns', 'n', 'vn'))
-    # print(keyWords)
+    # keyWords = analyse.extract_tags(tmp, topK=5, allowPOS=('ns', 'n'))
+    keyWords = []
+    count = 0
+    for i in frequency_sort(jieba.lcut(tmp)):
+        if count == 5: break
+        elif (not i.isspace()) and (len(i) > 1):
+            count += 1
+            keyWords.append(i)
+
     try:
         keyWords.remove(keyword)
     except:
         pass
     
     return keyWords
-    # return ['皇马', '皇家', '射手榜', '利云度', '云度']
